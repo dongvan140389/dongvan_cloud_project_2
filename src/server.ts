b4124f6ff65365182ee7e4ac20774542337c963e
dong.thai <>
2022-04-08 15:00:50 +0700

    implement new api to filter image

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +30,37 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage", (req: Request, res: Response) => {
+    let imageUrl: string = req.query.image_url;
+    console.log("check url = " + imageUrl);
+    let imageList: Array<string> = [];
+
+    if (!imageUrl) {
+      return res.status(404)
+        .send(`Image url not found...!`);
+    }
+
+    filterImageFromURL(imageUrl).then(imagePath => {
+      return res.status(200).sendFile(imagePath, (error) => {
+        imageList.push(imagePath);
+        deleteLocalFiles(imageList);
+      });
+    }).catch(_ => {
+      return res.status(400)
+        .send("Filter Image error!");
+    });
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  }); ``
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log("server running http://localhost:${port}");
+    console.log("press CTRL+C to stop server");
+  });
 })();
